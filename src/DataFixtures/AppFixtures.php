3afa86cd813ed14cc59c7eeadf64ec9c6ca6bb8f
Charly Guerin <lannister135@gmail.com>
2023-02-02 18:10:47 +0100

    data 2.0

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Faker\Factory;
+
+class AppFixtures extends Fixture
+{
+    private UserPasswordHasherInterface $hasher;
+
+    public function __construct(UserPasswordHasherInterface $hasher)
+    {
+        $this->hasher = $hasher;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $faker = Factory::create('fr_FR');
+
+        $user = new User();
+
+        $user->setEmail('user@test.com')
+             ->setPrenom($faker->firstName())
+             ->setNom($faker->lastName());
+
+        $password = $this->hasher->hashPassword($user, 'pass_1234');
+        $user->setPassword($password);
+
+        $manager->persist($user);
+
+        $manager->flush();
+    }
+}
