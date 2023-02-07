06a3d3c72fb3f72ec514952fa9cb1369cec3a2b4
Charly Guerin <lannister135@gmail.com>
2023-02-07 21:34:19 +0100

    création entité contact

---
+++ src/Entity/Reservation.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ReservationRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ReservationRepository::class)]
+class Reservation
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne]
+    private ?Etablissement $Etablissement = null;
+
+    #[ORM\ManyToOne]
+    private ?Suite $Suite = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $dateDeDebut = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $dateDeFin = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEtablissement(): ?Etablissement
+    {
+        return $this->Etablissement;
+    }
+
+    public function setEtablissement(?Etablissement $Etablissement): self
+    {
+        $this->Etablissement = $Etablissement;
+
+        return $this;
+    }
+
+    public function getSuite(): ?Suite
+    {
+        return $this->Suite;
+    }
+
+    public function setSuite(?Suite $Suite): self
+    {
+        $this->Suite = $Suite;
+
+        return $this;
+    }
+
+    public function getDateDeDebut(): ?\DateTimeInterface
+    {
+        return $this->dateDeDebut;
+    }
+
+    public function setDateDeDebut(\DateTimeInterface $dateDeDebut): self
+    {
+        $this->dateDeDebut = $dateDeDebut;
+
+        return $this;
+    }
+
+    public function getDateDeFin(): ?\DateTimeInterface
+    {
+        return $this->dateDeFin;
+    }
+
+    public function setDateDeFin(\DateTimeInterface $dateDeFin): self
+    {
+        $this->dateDeFin = $dateDeFin;
+
+        return $this;
+    }
+}
